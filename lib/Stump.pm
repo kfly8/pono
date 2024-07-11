946603012fc2dc6250e1e7b6ae1040fb07fd7719
kobaken <kentafly88@gmail.com>
2024-07-11 10:07:24 +0900

    minil new

---
+++ lib/Stump.pm
@@ -0,0 +1,39 @@
+package Stump;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Stump - It's new $module
+
+=head1 SYNOPSIS
+
+    use Stump;
+
+=head1 DESCRIPTION
+
+Stump is ...
+
+=head1 LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kobaken E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
