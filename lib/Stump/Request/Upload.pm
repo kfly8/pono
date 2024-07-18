d893fb5ce0fd272782ceedc19f94e07ad6b95e57
kobaken <kentafly88@gmail.com>
2024-07-18 21:31:40 +0900

    Upload class

--- lib/Stump/Request/Upload.pm
+++ lib/Stump/Request/Upload.pm
@@ -3,6 +3,24 @@ use utf8;
 use experimental qw(class);
 
 class Stump::Request::Upload {
-    # TODO
+    field $headers  :param :reader;
+    field $tempname :param :reader;
+    field $size     :param :reader;
+    field $filename :param :reader;
+    field $basename;
 
+    method path() { $tempname }
+    method content_type { $headers->content_type(@_) }
+
+    method basename() {
+        unless (defined $basename) {
+            require File::Spec::Unix;
+            my $bn = $filename;
+            $bn =~ s|\\|/|g;
+            $bn = ( File::Spec::Unix->splitpath($basename) )[2];
+            $bn =~ s|[^\w\.-]+|_|g;
+            $basename = $bn;
+        }
+        $basename;
+    }
 }
